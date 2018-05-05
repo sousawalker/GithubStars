7270d41b377f116f2a8c2a231cfff30dfd01840b
Walker Sousa <sousa.walker@gmail.com>
2018-05-05 06:44:58 -0300

    Adição de modal para preencher o token.

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  @ViewChild('usernameInput') usernameInput;
+  @ViewChild('tokenInput') tokenInput;
+  @ViewChild('tokenModal') tokenModal;
 
-  constructor() { }
+  token = localStorage.getItem('token');
+
+  constructor( private router: Router ) { }
 
   ngOnInit() {
   }
 
+  submitUsername(event) {
+    event.preventDefault();
+
+    localStorage.setItem('username', this.usernameInput.nativeElement.value);
+
+    this.router.navigate(['/username/'+localStorage.getItem('username')]);
+  }
+
+  submitToken(event) {
+    event.preventDefault();
+
+    localStorage.setItem('token', this.tokenInput.nativeElement.value);
+
+    this.tokenModal.nativeElement.classList.add('hidden');
+  }
 }
