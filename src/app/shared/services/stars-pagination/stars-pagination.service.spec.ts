8ebf9bd5ee74d9d720600cd61db826ec11b2a3ee
Walker Sousa <sousa.walker@gmail.com>
2018-05-07 00:38:27 -0300

    Adição de serviço star-pagination.

---
+++ src/app/shared/services/stars-pagination/stars-pagination.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { StarsPaginationService } from './stars-pagination.service';
+
+describe('StarsPaginationService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [StarsPaginationService]
+    });
+  });
+
+  it('should be created', inject([StarsPaginationService], (service: StarsPaginationService) => {
+    expect(service).toBeTruthy();
+  }));
+});
